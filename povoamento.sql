8d8ca63eaa3f12acf276aada2b16cdd27afb53e6
Sheyla Lima <44805014+slls-sheyla@users.noreply.github.com>
2021-07-13 20:55:02 -0300

    Update povoamento.sql

--- povoamento.sql
+++ povoamento.sql
@@ -14,17 +14,31 @@ INSERT INTO pessoa (cpf, nome) VALUES (95871,'Samira Matias');
 INSERT INTO pessoa (cpf, nome) VALUES (56238,'Hernando Rodrigues');
 INSERT INTO pessoa (cpf, nome) VALUES (13519,'Camila Arruda');
 INSERT INTO pessoa (cpf, nome) VALUES (57324,'Silvio Castro');
-INSERT INTO pessoa (cpf, nome) VALUES (65432,'');
-INSERT INTO pessoa (cpf, nome) VALUES (69743,'');
-INSERT INTO pessoa (cpf, nome) VALUES (59717,'');
-INSERT INTO pessoa (cpf, nome) VALUES (47120,'');
+INSERT INTO pessoa (cpf, nome) VALUES (12345678902, 'Breno');
 
 -- Paciente
+INSERT INTO paciente VALUES (00011122244, 'Igor', 1, 4546, 'Bradesco');
+
+
 -- Medico
+INSERT INTO medico VALUES (00000000000, 'Eduardo', 3565, 'Oftalmologia', 11111111111);
 
 -- Povoando consulta
+INSERT INTO consulta VALUES ('https://meet.google.com/gig-ciiu-exj', 00000000000);
+
 -- Povoando marcacao
+INSERT INTO marcacao VALUES (2021-07-13);
+
+
 -- Povoando telefone
+INSERT INTO telefone VALUES (55555555555, '(81)9 9999-5555');
+
+
 -- Povoando medicamento
+INSERT INTO medicamento VALUES (55, 'Paracetamol');
+
 -- Povoando exame
+INSERT INTO exame VALUES (96385274112, 35, 'Acuidade Visual', 'Ok', 2021-07-13);
+
 -- Povoando receita
+INSERT INTO receita VALUES (546, 'Sheyla Lima', 2021-07-13);
