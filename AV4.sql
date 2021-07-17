3804c69a74b63b8ac60633a831a937db08c6d878
Breno <bor@cin.ufpe.br>
2021-07-17 17:04:47 -0300

    Init script for tables consulting

---
+++ AV4.sql
@@ -0,0 +1,7 @@
+--Script da AV4
+
+--Selecionar todas as consultas de 2021, e seus respectivos pacientes, médicos, e links da chamada
+SELECT DISTINCT paciente.nome, medico.nome, consulta.link_chamada, receita.data_hora_marcacao
+FROM paciente, medico, consulta, receita, marcacao
+WHERE (consulta.cpf_paciente = paciente.cpf) AND (consulta.cpf_medico = medico.cpf) AND (medico.nome = receita.assinatura)
+AND (TO_CHAR(receita.data_hora_marcacao, 'yyyy') = '2021') ORDER BY receita.data_hora_marcacao;
