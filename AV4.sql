1cd308e7a9ef83f0c75f2acab02fbf19ecb19d77
Breno <bor@cin.ufpe.br>
2021-07-18 20:27:51 -0300

    Add COUNT and LEFT OUTER JOIN

--- AV4.sql
+++ AV4.sql
@@ -1,22 +1,37 @@
---Script da AV4
+-- Script da AV4
 
---Alterar a coluna nome para nome2 e vice-versa
+-- Alterar a coluna nome para nome2 e vice-versa
 ALTER TABLE paciente RENAME COLUMN nome TO nome2;
 ALTER TABLE paciente RENAME COLUMN nome2 TO nome;
 
 -- Cruzar dados dos exames com os pacientes
---INNER JOIN
+-- INNER JOIN
 SELECT * FROM exame INNER JOIN paciente
 ON (exame.cpf_paciente = paciente.cpf);
 
---Selecionar médico chefe da clínica ou selecionar empregados
---IS NULL
+-- Selecionar todos os números de telefone, onde na coluna da esquerda são adicionados
+-- apenas médicos
+-- LEFT OUTER JOIN
+SELECT nome, num_telefone FROM telefone LEFT OUTER JOIN medico
+ON (cpf_pessoa = medico.cpf);
+
+-- Selecionar médico chefe da clínica ou selecionar empregados
+-- IS NULL
 SELECT nome, cpf, crm FROM medico WHERE cpf_chefe IS NULL;
---IS NOT NULL
+-- IS NOT NULL
 SELECT nome, cpf, crm FROM medico WHERE cpf_chefe IS NOT NULL; 
 
---Selecionar todas as consultas de 2021, e seus respectivos pacientes, médicos, e links da chamada
---ORDER BY
+
+-- Contar o número total de pessoas cadastradas
+-- ou o número de pacientes cadastrados
+-- ou o número de médicos cadastrados
+-- COUNT
+SELECT COUNT(nome) FROM pessoa;
+SELECT COUNT(paciente.nome) FROM paciente INNER JOIN pessoa ON pessoa.cpf = paciente.cpf;
+SELECT COUNT(medico.nome) FROM medico INNER JOIN pessoa ON pessoa.cpf = medico.cpf;
+
+-- Selecionar todas as consultas de 2021, e seus respectivos pacientes, médicos, e links da chamada
+-- ORDER BY
 SELECT DISTINCT paciente.nome, medico.nome, consulta.link_chamada, receita.data_hora_marcacao
 FROM paciente, medico, consulta, receita, marcacao
 WHERE (consulta.cpf_paciente = paciente.cpf) AND (consulta.cpf_medico = medico.cpf) AND (medico.nome = receita.assinatura)
