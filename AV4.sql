53058bd73b510c7cf9b530039ebfa8df93b0b862
Breno Rodrigues <42875812+BrenoOR@users.noreply.github.com>
2021-07-18 19:49:04 -0300

    Update on querries description

--- AV4.sql
+++ AV4.sql
@@ -5,14 +5,18 @@ ALTER TABLE paciente RENAME COLUMN nome TO nome2;
 ALTER TABLE paciente RENAME COLUMN nome2 TO nome;
 
 -- Cruzar dados dos exames com os pacientes
+--INNER JOIN
 SELECT * FROM exame INNER JOIN paciente
 ON (exame.cpf_paciente = paciente.cpf);
 
 --Selecionar médico chefe da clínica ou selecionar empregados
+--IS NULL
 SELECT nome, cpf, crm FROM medico WHERE cpf_chefe IS NULL;
+--IS NOT NULL
 SELECT nome, cpf, crm FROM medico WHERE cpf_chefe IS NOT NULL; 
 
 --Selecionar todas as consultas de 2021, e seus respectivos pacientes, médicos, e links da chamada
+--ORDER BY
 SELECT DISTINCT paciente.nome, medico.nome, consulta.link_chamada, receita.data_hora_marcacao
 FROM paciente, medico, consulta, receita, marcacao
 WHERE (consulta.cpf_paciente = paciente.cpf) AND (consulta.cpf_medico = medico.cpf) AND (medico.nome = receita.assinatura)
