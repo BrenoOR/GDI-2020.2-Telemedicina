bfff66bf4e3524828311fd9db2400c6851e89778
Sheyla Lima <44805014+slls-sheyla@users.noreply.github.com>
2021-08-09 20:21:10 -0300

    Update AV5.sql

---
+++ AV5.sql
@@ -0,0 +1,25 @@
+CREATE TYPE tp_pessoa AS OBJECT (
+
+	nome VARCHAR2(100),
+	idade NUMBER,
+	telefone NUMBER,
+	CPF VARCHAR2(14),	
+
+) NOT FINAL NOT INSTANTIABLE;
+
+CREATE TYPE tp_paciente UNDER tp_pessoa (
+
+	n_sus NUMBER,
+	plano_saude VARCHAR2(100)
+);
+
+
+CREATE TYPE tp_medico UNDER tp_pessoa (
+
+	crm NUMBER,
+	especialidade VARCHAR2(30),
+);
+
+CREATE TABLE tb_paciente OF tp_paciente;
+
+CREATE TABLE tb_medico OF tp_medico;
