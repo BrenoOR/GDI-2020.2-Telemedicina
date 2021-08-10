179351d2bc29ced2add85dd456d2c152bffa2ad2
Sheyla Lima <44805014+slls-sheyla@users.noreply.github.com>
2021-08-10 11:56:56 -0300

    Update AV5.sql

--- AV5.sql
+++ AV5.sql
@@ -1,3 +1,5 @@
+-- Criando o tipo tp_pessoa
+
 CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
 
 	nome VARCHAR2(100),
@@ -7,25 +9,137 @@ CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
 
 ) NOT FINAL;
 
+-- Criando a tabela de pessoa
+
+CREATE TABLE tb_pessoa OF tp_pessoa (
+
+  cpf PRIMARY KEY
+
+);
+                                 
+
+-- Criando o tipo tp_paciente e fazendo o mesmo herdar os atributos de tp_pessoa
 CREATE OR REPLACE TYPE tp_paciente UNDER tp_pessoa (
 
 	n_sus NUMBER,
 	plano_saude VARCHAR2(100)
+
 );
 
+-- Criando a tabela de paciente e colocando cpf como chave primária
+CREATE TABLE tb_paciente OF tp_paciente (
 
+  CPF PRIMARY KEY;
+
+);                           
+
+
+-- Criando o tipo tp_medico e fazendo o mesmo herdar os atributos de tp_pessoa
 CREATE OR REPLACE TYPE tp_medico UNDER tp_pessoa (
 
 	crm NUMBER,
 	especialidade VARCHAR2(30)
+
 );
 
-CREATE TABLE tb_paciente OF tp_paciente;
+-- Criando a tabela de medico e colocando cpf como chave primária
+CREATE TABLE tb_medico OF tp_medico(
 
-//Referencias para endereco
+  CPF PRIMARY KEY;
+
+);
+
+
+-- Criando o tipo tp_marcacao
+CREATE OR REPLACE TYPE tp_marcacao AS OBJECT (
+
+  data_hora DATE
+
+);
+
+-- Criando a tabela de marcacao e colocando data_hora como chave primária
+CREATE TABLE tb_marcacao OF tp_marcacao (
+
+  data_hora PRIMARY KEY
+
+)
+
+
+-- Criando o tipo tp_consulta
+CREATE OR REPLACE TYPE tp_consulta AS OBJECT (
+
+  link_chamada VARCHAR2(50),
+  cpf_medico VARCHAR2(14),
+  cpf_paciente VARCHAR2(14)
+
+);
+
+-- Criando a tabela de consulta e colocando link_chamada como chave primária
+CREATE TABLE tb_consulta OF tp_consulta (
+
+  link_chamada PRIMARY KEY
+
+)
+
+
+-- Criando o tipo tp_receita
+CREATE OR REPLACE TYPE tp_receita AS OBJECT (
+
+  cod_verificacao NUMBER,
+  assinatura VARCHAR2(30)
+                      
+);
+
+-- Criando a tabela de receita e colocando cod_verificacao como chave primária
+CREATE TABLE tb_receita OF tp_receita (
+
+  cod_verificacao PRIMARY KEY
+
+)
+
+
+-- Criando o tipo tp_exame
+CREATE OR REPLACE TYPE tp_exame AS OBJECT (
+
+  cpf_paciente VARCHAR2(14),
+  numero NUMBER,
+  tipo VARCHAR2(50),
+  resultado VARCHAR2(100),
+  data_hora_marcacao DATE
+                      
+);
+
+-- Criando a tabela de exame e colocando numero como chave primária
+CREATE TABLE tb_exame OF tp_exame (
+
+  numero PRIMARY KEY
+
+)
+
+
+-- Criando o tipo tp_medicamento
+CREATE OR REPLACE TYPE tp_medicamento AS OBJECT (
+
+  nome_medicamento VARCHAR2(30),
+  laboratorio VARCHAR2(30),
+  contraindicacao VARCHAR2(50),
+  data_fabricacao DATE,
+  data_validade DATE, 
+  cod_verif_receita NUMBER
+                      
+);
+
+-- Criando a tabela de medicamento e colocando nome_medicamento como chave primária
+CREATE TABLE tb_medicamento OF tp_medicamento (
+
+  nome_medicamento PRIMARY KEY
+
+)
+
+-- Referencias para endereco
 SELECT REF(e) FROM endereco e;
 
-//Valor para
+-- Valor para
 SELECT VALUE(p) FROM tb_paciente;
 VALUE (P) (nome,cpf);
 
